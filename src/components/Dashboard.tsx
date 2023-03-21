6f9b2e84f3d67347bef1631f1751ffa9a774649a
Sammy-Jo Wymer <sammy-jo.wymer@diconium.com>
2023-03-21 17:09:41 +0100

    feat/add-basic-dashboard-and-styling

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+
+export default function Dashboard() {
+  const totalValue = 200;
+  const foodValue = 20;
+  const clothesValue = 50;
+  const billsValue = 130;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This month you have spent {totalValue} Euro</p>
+      <p>Food: {foodValue} Euro</p>
+      <p>Clothes: {clothesValue} Euro</p>
+      <p>Bills: {billsValue} Euro</p>
+      <Button type="Add" />
+      <Button type="Remove" />
+    </div>
+  );
+}
